cae51f64b8b859fd4d051efc357658a624c95d8b
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-03-06 01:13:40 +0530

    getting user selected file

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const fileInput = document.querySelector(".file-input"),
+previewImg = document.querySelector('.preview-img img');
 chooseImgBtn = document.querySelector('.choose-img');
+
 
 const loadImage = ()=> {
     let file = fileInput.files[0]; // getting user selected file
-    console.log(file);
+    if(!file) return; //return if user hasn't selected file
+    previewImg.src = URL.createObjectURL(file);   
+    // console.log(file);
 }
 
 fileInput.addEventListener("change", loadImage);
