9cae305813d7dabc6db5f4e02a64106786324949
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-03-06 00:55:23 +0530

    file-input as image on click choose img button

---
+++ script.js
@@ -0,0 +1,4 @@
+const fileInput = document.querySelector(".file-input"),
+chooseImgBtn = document.querySelector('.choose-img');
+
+chooseImgBtn.addEventListener('click', ()=> fileInput.click());   //file-input as image on click choose img button
