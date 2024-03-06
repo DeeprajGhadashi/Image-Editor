056adc706066f441a74ec872306a0edbdf065927
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-03-06 11:15:58 +0530

    filter apply on image is done

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ chooseImgBtn = document.querySelector('.choose-img');
 
 let brightness = 100, saturation = 100, inversion = 0 , grayscale = 0;
 
+const  applyFilters = () => {
+    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
+}
+
 const loadImage = ()=> {
     let file = fileInput.files[0]; // getting user selected file
     if(!file) return; //return if user hasn't selected file
@@ -23,6 +27,24 @@ filterOptions.forEach(option => {
         document.querySelector('.filter .active').classList.remove('active');
         option.classList.add('active');
         filterName.innerText = option.innerText;
+
+        if(option.id === 'brightness') {
+            filterSlider.max ='200';
+            filterSlider.value = brightness ;
+            filterValue.innerText = `${brightness}%`;
+        } else if(option.id === 'saturation') {
+            filterSlider.max ='200';
+            filterSlider.value = saturation;
+            filterValue.innerText = `${saturation}%`;
+        } else if(option.id === 'inversion') {
+            filterSlider.max ='100';
+            filterSlider.value = inversion;
+            filterValue.innerText = `${inversion}%`;
+        } else {
+            filterSlider.max ='100';
+            filterSlider.value = grayscale;
+            filterValue.innerText = `${grayscale}%`;
+        }
     })
 })
 
@@ -33,12 +55,13 @@ const updateFilter = () => {
     if(selectedFilter.id === "brightness") {
         brightness = filterSlider.value;
     }else if(selectedFilter.id === "saturation") {
-        brightness = filterSlider.value;
+        saturation = filterSlider.value;
     } else if(selectedFilter.id === "inversion") {
-        brightness = filterSlider.value;
+        inversion = filterSlider.value;
     } else {
-        brightness = filterSlider.value;
+        grayscale = filterSlider.value;
     } 
+    applyFilters();
 }
 
 fileInput.addEventListener("change", loadImage);
